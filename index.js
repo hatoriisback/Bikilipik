f328d7ab4708082404d722741cc72981bf7e02af
Hendy <hendy.findme@gmail.com>
2021-10-02 18:26:21 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+let name = 'Hendy';
+let role = 'member lama';
+let birthMonth = 'januari';
+
+if (name === '') {
+    console.log('String tidak boleh kosong');
+} else if (role === 'member baru') {
+    if (birthMonth === 'januari' || birthMonth === 'Januari') {
+        console.log(`Selamat ${name}!. Anda berhak mendapatkan voucher diskon sebesar 50%!`);
+    } else {
+        console.log(`Maaf ${name}, voucher diskon hanya untuk yang lahir di bulan Januari.`);
+    }
+} else if (role === 'member lama') {
+    if (birthMonth === 'januari' || birthMonth === 'Januari') {
+        console.log(`Selamat ${name}!. Anda berhak mendapatkan voucher diskon sebesar 30%!`);
+    } else {
+        console.log(`Maaf ${name}, voucher diskon hanya untuk yang lahir di bulan Januari.`);
+    }
+} else if (role !== 'member lama' || role !== 'member baru') {
+console.log(`Selamat datang di Bikilipik, ${name}. Mohon maaf untuk mendapatkan voucher diskon, anda wajib mendaftar dahulu.`);
+}
